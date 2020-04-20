c69130a4aff5ac6a0d5d358d421c3471872acbb3
Katarzyna Kujawa <katarzyna.kujawa@intel.com>
2020-04-20 14:03:23 +0200

    Move calculating of refernced bytes into libcontainer
    Signed-off-by: Katarzyna Kujawa <katarzyna.kujawa@intel.com>

--- container/libcontainer/handler.go
+++ container/libcontainer/handler.go
@@ -30,7 +30,6 @@ import (
 
 	"github.com/google/cadvisor/container"
 	info "github.com/google/cadvisor/info/v1"
-	"github.com/google/cadvisor/wss"
 	"golang.org/x/sys/unix"
 
 	"bytes"
@@ -40,17 +39,25 @@ import (
 	"k8s.io/klog/v2"
 )
 
+var (
+	whitelistedUlimits = [...]string{"max_open_files"}
+	wssResetInterval   = flag.Uint64("wss_reset_interval", 5, "Reset interval for working set size metric, number of measurement cycles after which referenced bytes are cleared")
+
+	smapsFilePathPattern     = "/proc/%d/smaps"
+	clearRefsFilePathPattern = "/proc/%d/clear_refs"
+
+	referencedRegexp = regexp.MustCompile(`Referenced:\s*([0-9]+)\s*kB`)
+	isDigitRegExp    = regexp.MustCompile("\\d+")
+)
+
 type Handler struct {
 	cgroupManager   cgroups.Manager
 	rootFs          string
 	pid             int
 	includedMetrics container.MetricSet
 	pidMetricsCache map[int]*info.CpuSchedstat
-	wssCycles       uint64
+	cycles          uint64
 }
-
-var whitelistedUlimits = [...]string{"max_open_files"}
-var wssResetInterval = flag.Uint64("wss_reset_interval", 5, "Reset interval for working set size metric, number of measurement cycles after which referenced bytes are cleared")
 
 func NewHandler(cgroupManager cgroups.Manager, rootFs string, pid int, includedMetrics container.MetricSet) *Handler {
 	return &Handler{
@@ -86,12 +93,12 @@ func (h *Handler) GetStats() (*info.ContainerStats, error) {
 	}
 
 	if h.includedMetrics.Has(container.WssMetric) {
-		h.wssCycles++
+		h.cycles++
 		pids, err := h.cgroupManager.GetPids()
 		if err != nil {
 			klog.V(4).Infof("Could not get PIDs for container %d: %v", h.pid, err)
 		} else {
-			stats.Wss, err = wss.GetStat(pids, h.wssCycles, *wssResetInterval)
+			stats.Wss, err = referencedBytesStat(pids, h.cycles, *wssResetInterval)
 			if err != nil {
 				klog.V(4).Infof("Unable to get working set size: %v", err)
 			}
@@ -333,6 +340,90 @@ func schedulerStatsFromProcs(rootFs string, pids []int, pidMetricsCache map[int]
 		schedstats.RunTime += v.RunTime
 	}
 	return schedstats, nil
+}
+
+func referencedBytesStat(pids []int, cycles uint64, resetInterval uint64) (uint64, error) {
+	referencedKBytes, err := getReferencedBytes(pids)
+	if err != nil {
+		return uint64(0), err
+	}
+
+	err = clearReferencedBytes(pids, cycles, resetInterval)
+	if err != nil {
+		return uint64(0), err
+	}
+	return referencedKBytes * 1024, nil
+}
+
+func getReferencedBytes(pids []int) (uint64, error) {
+	referencedKBytes := uint64(0)
+	readSmapsContent := false
+	foundMatch := false
+	for _, pid := range pids {
+		smapsFilePath := fmt.Sprintf(smapsFilePathPattern, pid)
+		smapsContent, err := ioutil.ReadFile(smapsFilePath)
+		if err != nil {
+			klog.V(3).Infof("Cannot read %s file, err: %s", smapsFilePath, err)
+			if os.IsNotExist(err) {
+				continue //smaps file does not exists for all PIDs
+			}
+			return 0, err
+		}
+		readSmapsContent = true
+
+		allMatches := referencedRegexp.FindAllSubmatch(smapsContent, -1)
+		if len(allMatches) == 0 {
+			klog.V(3).Infof("Not found any information about referenced bytes in %s file", smapsFilePath)
+			continue // referenced bytes may not exist in smaps file
+		}
+
+		for _, matches := range allMatches {
+			if len(matches) != 2 {
+				return 0, fmt.Errorf("failed to match regexp in output: %s", string(smapsContent))
+			}
+			foundMatch = true
+			referenced, err := strconv.ParseUint(string(matches[1]), 10, 64)
+			if err != nil {
+				return 0, err
+			}
+			referencedKBytes += referenced
+		}
+	}
+
+	if len(pids) != 0 {
+		if !readSmapsContent {
+			klog.Warningf("Cannot read smaps files for any PID from %s", "CONTAINER")
+		} else if !foundMatch {
+			klog.Warningf("Not found any information about referenced bytes in smaps files for any PID from %s", "CONTAINER")
+		}
+	}
+	return referencedKBytes, nil
+}
+
+func clearReferencedBytes(pids []int, cycles uint64, resetInterval uint64) error {
+	if resetInterval == 0 {
+		return fmt.Errorf("Incorrect of reset interval for wss, ResetInterval: %d", resetInterval)
+	}
+
+	if cycles%resetInterval == 0 {
+		for _, pid := range pids {
+			clearRefsFilePath := fmt.Sprintf(clearRefsFilePathPattern, pid)
+			clerRefsFile, err := os.OpenFile(clearRefsFilePath, os.O_WRONLY, 0644)
+			if err != nil {
+				// clear_refs file may not exist for all PIDs
+				continue
+			}
+			_, err = clerRefsFile.WriteString("1\n")
+			if err != nil {
+				return err
+			}
+			err = clerRefsFile.Close()
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 func networkStatsFromProc(rootFs string, pid int) ([]info.InterfaceStats, error) {
